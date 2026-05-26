adecba94da28212265959831dc0bdcc3ca92af9f
DarkSoulWind <salmanazhan@gmail.com>
2026-05-26 15:14:25 +0100

    Fix Astro path aliases for Cloudflare build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,20 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import { fileURLToPath } from "node:url";
+
+const alias = (path) => fileURLToPath(new URL(path, import.meta.url));
 
 export default defineConfig({
 	site: "https://malayassociation.co.uk",
 	output: "static",
 	integrations: [sitemap()],
+	vite: {
+		resolve: {
+			alias: {
+				"@components": alias("./src/components"),
+				"@layouts": alias("./src/layouts"),
+				"@styles": alias("./src/styles"),
+			},
+		},
+	},
 });
