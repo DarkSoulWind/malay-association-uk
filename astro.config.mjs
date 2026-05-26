94ac2ae38a0b1563eba9ffb1688018c63d66fc09
DarkSoulWind <salmanazhan@gmail.com>
2026-05-26 15:06:32 +0100

    Fixing build errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
-
 export default defineConfig({
 	site: "https://malayassociation.co.uk",
 	output: "static",
